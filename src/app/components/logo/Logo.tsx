1689a269c14f91454bc949fb649d4f9ca56a9d23
keti <qeti.tsiklauri320@ens.tsu.edu.ge>
2025-09-28 12:58:18 +0400

    fix

--- src/app/components/logo/Logo.tsx
+++ src/app/components/logo/Logo.tsx
@@ -7,7 +7,7 @@ export default function Logo() {
   return (
     <Link href="/">
       <Image
-        src="icon-logo.svg"
+        src="/icon-logo.svg"
         alt="Logo"
         width={180}
         height={24}
